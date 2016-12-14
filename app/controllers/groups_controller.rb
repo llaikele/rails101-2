3441750989309da0a95cf1103c2bdb1483391016
你的名字 <email>
2016-12-14 23:27:19 +0800

    6.3.6

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -12,6 +12,7 @@ class GroupsController < ApplicationController
 
   def show
     @group = Group.find(params[:id])
+    @posts = @group.posts
   end
 
   def edit
@@ -44,7 +45,7 @@ end
 
 
  def destroy
-   
+
    @group.destroy
 
    redirect_to groups_path, alert: "Group deleted"
